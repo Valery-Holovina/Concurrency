0d0e081c0b487db9e42b945001852f1da75dca0e
Valery-Holovina <valery.holovina09@gmail.com>
2025-11-17 10:57:42 +0200

    Initial Commit

---
+++ Concurrency/ConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcurrencyApp.swift
+//  Concurrency
+//
+//  Created by Valery on 17.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct ConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
