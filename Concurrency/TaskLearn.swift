674fb4ed9d0ad9ea6e89b44bb1bfebeba659b712
Valery-Holovina <valery.holovina09@gmail.com>
2025-11-20 12:08:37 +0200

    TaskLearn

--- Concurrency/TaskLearn.swift
+++ Concurrency/TaskLearn.swift
@@ -11,17 +11,53 @@ import SwiftUI
 @Observable class TaskLearnViewModel{
     
     var image : UIImage? = nil
-    
+    var image2 : UIImage? = nil
     
     func fetchImgae() async{
+        try? await Task.sleep(nanoseconds: 5_000_000_000)
         do {
             
             guard let url = URL(string: "https://picsum.photos/200") else {return}
             let (data,_) = try await URLSession.shared.data(from: url, delegate: nil)
             let image = UIImage(data: data)
-            self.image = image
+            await MainActor.run {
+                self.image = image
+            }
+          
         } catch  {
             print(error.localizedDescription)
+        }
+    }
+    
+    
+    
+    func fetchImgae2() async{
+        do {
+            
+            guard let url = URL(string: "https://picsum.photos/200") else {return}
+            let (data,_) = try await URLSession.shared.data(from: url, delegate: nil)
+            let image = UIImage(data: data)
+            await MainActor.run {
+                self.image2 = image
+            }
+        } catch  {
+            print(error.localizedDescription)
+        }
+    }
+}
+
+
+struct TaskLearnHomeView: View{
+    var body: some View{
+        NavigationStack{
+            ZStack{
+                NavigationLink {
+                    TaskLearn()
+                } label: {
+                    Text("Click me")
+                }
+
+            }
         }
     }
 }
@@ -29,6 +65,7 @@ import SwiftUI
 struct TaskLearn: View {
     
     private var vm = TaskLearnViewModel()
+//    @State private var  fetchTask: Task<(), Never>? = nil
     
     var body: some View {
         VStack(spacing: 40) {
@@ -38,14 +75,49 @@ struct TaskLearn: View {
                     .scaledToFit()
                     .frame(width: 200, height: 200)
             }
-        }
-        .onAppear{
-            Task{
-                await vm.fetchImgae()
+            if let image2 = vm.image2{
+                Image(uiImage: image2)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
             }
+
+        }
+        // new version
+        .task {
+            await vm.fetchImgae()
+//            try Task.checkCancellation() (not here but in tasks  that are long (loops))
+        }
+        
+        // old version
+//        .onDisappear{
+//            fetchTask?.cancel() // to cancel task
+//        }
+//        .onAppear{
+//            fetchTask = Task{
+////                await Task.yield() // like sleep (to give other tasks to do first)
+//                await vm.fetchImgae()
+//             
+//            }
+        
+        
+        
+        
+        
+        
+//            Task{
+//                await vm.fetchImgae2()
+//            }
+            
+            //PRIORITY
+        // 1. high and userInitiated
+        // 2. medium
+        // 3. low
+        // 4. utitlity
+        // 5. background
         }
     }
-}
+//}
 
 #Preview {
     TaskLearn()
